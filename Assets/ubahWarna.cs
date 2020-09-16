cc16e1dcc25f8d8beb912eef29be1e49a99a4d92
Tomi Dwi Setyawan <tomidwisetyawan1@gmail.com>
2020-09-16 10:25:21 +0700

    Finish tugas 3

---
+++ Assets/ubahWarna.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ubahWarna : MonoBehaviour
+{
+    public void Merah()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Biru()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Hitam()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    public void Hijau()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void Kuning()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void Abu()
+    {
+        GetComponent<Renderer>().material.color = Color.grey;
+    }
+}
